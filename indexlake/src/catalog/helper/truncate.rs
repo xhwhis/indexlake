a17029b48b160356ad4ed900a8bb115787c1d9a7
Linwei Zhang <lewiszlw520@gmail.com>
2025-06-23 23:57:12 +0800

    Replace truncate with delete for sqlite3

--- indexlake/src/catalog/helper/truncate.rs
+++ indexlake/src/catalog/helper/truncate.rs
@@ -1,16 +1,34 @@
-use crate::ILResult;
 use crate::catalog::TransactionHelper;
+use crate::{CatalogDatabase, ILResult};
 
 impl TransactionHelper {
     pub(crate) async fn truncate_inline_row_table(&mut self, table_id: i64) -> ILResult<()> {
-        self.transaction
-            .execute_batch(&[format!("TRUNCATE TABLE indexlake_inline_row_{table_id}")])
-            .await
+        match self.database {
+            CatalogDatabase::Sqlite => {
+                self.transaction
+                    .execute_batch(&[format!("DELETE FROM indexlake_inline_row_{table_id}")])
+                    .await
+            }
+            CatalogDatabase::Postgres => {
+                self.transaction
+                    .execute_batch(&[format!("TRUNCATE TABLE indexlake_inline_row_{table_id}")])
+                    .await
+            }
+        }
     }
 
     pub(crate) async fn truncate_row_metadata_table(&mut self, table_id: i64) -> ILResult<()> {
-        self.transaction
-            .execute_batch(&[format!("TRUNCATE TABLE indexlake_row_metadata_{table_id}")])
-            .await
+        match self.database {
+            CatalogDatabase::Sqlite => {
+                self.transaction
+                    .execute_batch(&[format!("DELETE FROM indexlake_row_metadata_{table_id}")])
+                    .await
+            }
+            CatalogDatabase::Postgres => {
+                self.transaction
+                    .execute_batch(&[format!("TRUNCATE TABLE indexlake_row_metadata_{table_id}")])
+                    .await
+            }
+        }
     }
 }
