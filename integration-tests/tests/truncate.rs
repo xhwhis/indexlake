09932d0585747ade24bfe9f0411c7ad3e1ac7abe
Linwei Zhang <lewiszlw520@gmail.com>
2025-07-11 11:30:15 +0800

    Fix test

--- integration-tests/tests/truncate.rs
+++ integration-tests/tests/truncate.rs
@@ -1,12 +1,7 @@
-use arrow::array::{Int64Array, RecordBatch, StringArray};
-use arrow::datatypes::{DataType, Field, Schema};
-use indexlake::{
-    LakeClient,
-    catalog::Catalog,
-    storage::Storage,
-    table::{TableConfig, TableCreation},
-};
-use indexlake_integration_tests::utils::full_table_scan;
+use futures::TryStreamExt;
+use indexlake::table::TableScan;
+use indexlake::{LakeClient, catalog::Catalog, storage::Storage};
+use indexlake_integration_tests::data::prepare_testing_table;
 use indexlake_integration_tests::{
     catalog_postgres, catalog_sqlite, init_env_logger, storage_fs, storage_s3,
 };
@@ -25,46 +20,14 @@ async fn truncate_table(
     init_env_logger();
 
     let client = LakeClient::new(catalog, storage);
-
-    let namespace_name = "test_namespace";
-    client.create_namespace(namespace_name).await?;
-
-    let table_schema = Arc::new(Schema::new(vec![
-        Field::new("id", DataType::Int64, false),
-        Field::new("name", DataType::Utf8, false),
-    ]));
-    let table_name = "test_table";
-    let table_creation = TableCreation {
-        namespace_name: namespace_name.to_string(),
-        table_name: table_name.to_string(),
-        schema: table_schema.clone(),
-        config: TableConfig::default(),
-    };
-    client.create_table(table_creation).await?;
-
-    let table = client.load_table(namespace_name, table_name).await?;
-
-    let record_batch = RecordBatch::try_new(
-        table_schema.clone(),
-        vec![
-            Arc::new(Int64Array::from(vec![1, 2])),
-            Arc::new(StringArray::from(vec!["Alice", "Bob"])),
-        ],
-    )?;
-
-    table.insert(&record_batch).await?;
+    let table = prepare_testing_table(&client, "truncate_table").await?;
 
     table.truncate().await?;
 
-    let table_str = full_table_scan(&table).await?;
-    println!("{}", table_str);
-    assert_eq!(
-        table_str,
-        r#"+-------------------+----+------+
-| _indexlake_row_id | id | name |
-+-------------------+----+------+
-+-------------------+----+------+"#,
-    );
+    let scan = TableScan::default();
+    let stream = table.scan(scan).await?;
+    let batches = stream.try_collect::<Vec<_>>().await?;
+    assert_eq!(batches.len(), 0);
 
     Ok(())
 }
