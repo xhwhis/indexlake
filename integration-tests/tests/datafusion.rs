ad79c2b251a44ef754555a71db179c9f0a4a4aec
张林伟 <lewiszlw520@gmail.com>
2025-08-18 14:23:22 +0800

    Fix datafusion insert proto

--- integration-tests/tests/datafusion.rs
+++ integration-tests/tests/datafusion.rs
@@ -262,7 +262,7 @@ async fn datafusion_scan_serialization(
     let df = session.sql("SELECT * FROM indexlake_table").await?;
     let plan = df.create_physical_plan().await?;
     println!(
-        "{}",
+        "plan: {}",
         DisplayableExecutionPlan::new(plan.as_ref()).indent(true)
     );
 
@@ -273,7 +273,7 @@ async fn datafusion_scan_serialization(
     let new_plan: Arc<dyn ExecutionPlan> = PhysicalPlanNode::try_decode(&plan_buf)
         .and_then(|proto| proto.try_into_physical_plan(&session, &session.runtime_env(), &codec))?;
     println!(
-        "{}",
+        "deserialized plan: {}",
         DisplayableExecutionPlan::new(new_plan.as_ref()).indent(true)
     );
 
@@ -296,78 +296,78 @@ async fn datafusion_scan_serialization(
     Ok(())
 }
 
-// #[rstest::rstest]
-// #[case(async { catalog_sqlite() }, async { storage_fs() }, DataFileFormat::ParquetV2)]
-// // #[case(async { catalog_postgres().await }, async { storage_s3().await }, DataFileFormat::ParquetV1)]
-// // #[case(async { catalog_postgres().await }, async { storage_s3().await }, DataFileFormat::ParquetV2)]
-// // #[case(async { catalog_postgres().await }, async { storage_s3().await }, DataFileFormat::LanceV2_0)]
-// #[tokio::test(flavor = "multi_thread")]
-// async fn datafusion_insert_serialization(
-//     #[future(awt)]
-//     #[case]
-//     catalog: Arc<dyn Catalog>,
-//     #[future(awt)]
-//     #[case]
-//     storage: Arc<Storage>,
-//     #[case] format: DataFileFormat,
-// ) -> Result<(), Box<dyn std::error::Error>> {
-//     init_env_logger();
+#[rstest::rstest]
+#[case(async { catalog_sqlite() }, async { storage_fs() }, DataFileFormat::ParquetV2)]
+#[case(async { catalog_postgres().await }, async { storage_s3().await }, DataFileFormat::ParquetV1)]
+#[case(async { catalog_postgres().await }, async { storage_s3().await }, DataFileFormat::ParquetV2)]
+#[case(async { catalog_postgres().await }, async { storage_s3().await }, DataFileFormat::LanceV2_0)]
+#[tokio::test(flavor = "multi_thread")]
+async fn datafusion_insert_serialization(
+    #[future(awt)]
+    #[case]
+    catalog: Arc<dyn Catalog>,
+    #[future(awt)]
+    #[case]
+    storage: Arc<Storage>,
+    #[case] format: DataFileFormat,
+) -> Result<(), Box<dyn std::error::Error>> {
+    init_env_logger();
 
-//     let client = Client::new(catalog, storage);
-//     let table = prepare_simple_testing_table(&client, format).await?;
+    let client = Client::new(catalog, storage);
+    let table = prepare_simple_testing_table(&client, format).await?;
 
-//     let df_table = IndexLakeTable::new(Arc::new(table));
-//     let session = SessionContext::new();
-//     session.register_table("indexlake_table", Arc::new(df_table))?;
-//     let df = session
-//         .sql("INSERT INTO indexlake_table (name, age) VALUES ('Eve', 24)")
-//         .await?;
-//     let plan = df.create_physical_plan().await?;
-//     println!(
-//         "{}",
-//         DisplayableExecutionPlan::new(plan.as_ref()).indent(true)
-//     );
+    let df_table = IndexLakeTable::new(Arc::new(table));
+    let session = SessionContext::new();
+    session.register_table("indexlake_table", Arc::new(df_table))?;
+    let df = session
+        .sql("INSERT INTO indexlake_table (name, age) VALUES ('Eve', 24)")
+        .await?;
+    let plan = df.create_physical_plan().await?;
+    println!(
+        "plan: {}",
+        DisplayableExecutionPlan::new(plan.as_ref()).indent(true)
+    );
 
-//     let codec = IndexLakePhysicalCodec::new(Arc::new(client));
-//     let mut plan_buf: Vec<u8> = vec![];
-//     let plan_proto = PhysicalPlanNode::try_from_physical_plan(plan, &codec)?;
-//     plan_proto.try_encode(&mut plan_buf)?;
-//     let new_plan: Arc<dyn ExecutionPlan> = PhysicalPlanNode::try_decode(&plan_buf)
-//         .and_then(|proto| proto.try_into_physical_plan(&session, &session.runtime_env(), &codec))?;
-//     println!(
-//         "{}",
-//         DisplayableExecutionPlan::new(new_plan.as_ref()).indent(true)
-//     );
+    let codec = IndexLakePhysicalCodec::new(Arc::new(client));
+    let mut plan_buf: Vec<u8> = vec![];
+    let plan_proto = PhysicalPlanNode::try_from_physical_plan(plan, &codec)?;
+    plan_proto.try_encode(&mut plan_buf)?;
+    let new_plan: Arc<dyn ExecutionPlan> = PhysicalPlanNode::try_decode(&plan_buf)
+        .and_then(|proto| proto.try_into_physical_plan(&session, &session.runtime_env(), &codec))?;
+    println!(
+        "deserialized plan: {}",
+        DisplayableExecutionPlan::new(new_plan.as_ref()).indent(true)
+    );
 
-//     let batches = collect(new_plan, session.task_ctx()).await?;
-//     let table_str = pretty_format_batches(&batches)?.to_string();
-//     println!("{}", table_str);
-//     assert_eq!(
-//         table_str,
-//         r#"+-------+
-// | count |
-// +-------+
-// | 1     |
-// +-------+"#,
-//     );
+    let batches = collect(new_plan, session.task_ctx()).await?;
+    let table_str = pretty_format_batches(&batches)?.to_string();
+    println!("{}", table_str);
+    assert_eq!(
+        table_str,
+        r#"+-------+
+| count |
++-------+
+| 1     |
++-------+"#,
+    );
 
-//     let df = session.sql("SELECT * FROM indexlake_table").await?;
-//     let batches = df.collect().await?;
-//     let sorted_batch = sort_record_batches(&batches, INTERNAL_ROW_ID_FIELD_NAME)?;
-//     let table_str = pretty_format_batches(&vec![sorted_batch])?.to_string();
-//     println!("{}", table_str);
-//     assert_eq!(
-//         table_str,
-//         r#"+-------------------+---------+-----+
-// | _indexlake_row_id | name    | age |
-// +-------------------+---------+-----+
-// | 1                 | Alice   | 20  |
-// | 2                 | Bob     | 21  |
-// | 3                 | Charlie | 22  |
-// | 4                 | David   | 23  |
-// | 5                 | Eve     | 24  |
-// +-------------------+---------+-----+"#,
-//     );
+    let df = session.sql("SELECT * FROM indexlake_table").await?;
+    let batches = df.collect().await?;
+    let sorted_batch = sort_record_batches(&batches, INTERNAL_ROW_ID_FIELD_NAME)?;
+    let table_str = pretty_format_batches(&vec![sorted_batch])?.to_string();
+    println!("{}", table_str);
+    assert_eq!(
+        table_str,
+        r#"+-------------------+---------+-----+
+| _indexlake_row_id | name    | age |
++-------------------+---------+-----+
+| 1                 | Alice   | 20  |
+| 2                 | Bob     | 21  |
+| 3                 | Charlie | 22  |
+| 4                 | David   | 23  |
+| 5                 | Eve     | 24  |
++-------------------+---------+-----+"#,
+    );
 
-//     Ok(())
-// }
+    Ok(())
+}
--- integrations/datafusion/src/codec.rs
+++ integrations/datafusion/src/codec.rs
@@ -1,20 +1,34 @@
 use std::sync::Arc;
 
 use datafusion::{
-    error::DataFusionError, execution::FunctionRegistry, logical_expr::dml::InsertOp,
+    arrow::{
+        array::RecordBatch,
+        datatypes::Schema,
+        ipc::{reader::StreamReader, writer::StreamWriter},
+    },
+    catalog::memory::{DataSourceExec, MemorySourceConfig},
+    datasource::source::DataSource,
+    error::DataFusionError,
+    execution::FunctionRegistry,
+    logical_expr::dml::InsertOp,
     physical_plan::ExecutionPlan,
 };
 use datafusion_proto::{
     logical_plan::{
         DefaultLogicalExtensionCodec, from_proto::parse_exprs, to_proto::serialize_exprs,
     },
-    physical_plan::PhysicalExtensionCodec,
+    physical_plan::{
+        PhysicalExtensionCodec, from_proto::parse_physical_sort_exprs,
+        to_proto::serialize_physical_sort_exprs,
+    },
 };
 use indexlake::{Client, table::Table};
 use prost::Message;
 
 use crate::{
-    IndexLakeInsertExec, IndexLakeInsertExecNode, IndexLakeScanExec, IndexLakeScanExecNode,
+    IndexLakeInsertExec, IndexLakeInsertExecNode, IndexLakePhysicalPlanNode, IndexLakeScanExec,
+    IndexLakeScanExecNode, MemoryDatasourceNode,
+    index_lake_physical_plan_node::IndexLakePhysicalPlanType,
 };
 
 #[derive(Debug)]
@@ -35,41 +49,83 @@ impl PhysicalExtensionCodec for IndexLakePhysicalCodec {
         inputs: &[Arc<dyn ExecutionPlan>],
         registry: &dyn FunctionRegistry,
     ) -> Result<Arc<dyn ExecutionPlan>, DataFusionError> {
-        if let Ok(node) = IndexLakeScanExecNode::decode(buf) {
-            let table = load_table(&self.client, &node.namespace_name, &node.table_name)?;
-
-            let projection = parse_projection(node.projection.as_ref());
-            let filters = parse_exprs(&node.filters, registry, &DefaultLogicalExtensionCodec {})?;
-
-            Ok(Arc::new(IndexLakeScanExec::try_new(
-                Arc::new(table),
-                node.partition_count as usize,
-                projection,
-                filters,
-                node.limit.map(|l| l as usize),
-            )?))
-        } else if let Ok(node) = IndexLakeInsertExecNode::decode(buf) {
-            if inputs.len() != 1 {
-                return Err(DataFusionError::Internal(format!(
-                    "IndexLakeInsertExec requires exactly one input, got {}",
-                    inputs.len()
-                )));
-            }
-            let input = inputs[0].clone();
-
-            let table = load_table(&self.client, &node.namespace_name, &node.table_name)?;
-
-            let insert_op = parse_insert_op(node.insert_op)?;
-
-            Ok(Arc::new(IndexLakeInsertExec::try_new(
-                Arc::new(table),
-                input,
-                insert_op,
-            )?))
-        } else {
-            Err(DataFusionError::Execution(
-                "IndexLakePhysicalCodec failed to decode physical plan".to_string(),
+        let indexlake_node = IndexLakePhysicalPlanNode::decode(buf).map_err(|e| {
+            DataFusionError::Internal(format!(
+                "Failed to decode indexlake physical plan node: {e:?}"
             ))
+        })?;
+        let indexlake_plan = indexlake_node.index_lake_physical_plan_type.ok_or_else(|| {
+            DataFusionError::Internal(
+                "Failed to decode indexlake physical plan node due to physical plan type is none".to_string()
+            )
+        })?;
+
+        match indexlake_plan {
+            IndexLakePhysicalPlanType::Scan(node) => {
+                let table = load_table(&self.client, &node.namespace_name, &node.table_name)?;
+
+                let projection = parse_projection(node.projection.as_ref());
+                let filters =
+                    parse_exprs(&node.filters, registry, &DefaultLogicalExtensionCodec {})?;
+
+                Ok(Arc::new(IndexLakeScanExec::try_new(
+                    Arc::new(table),
+                    node.partition_count as usize,
+                    projection,
+                    filters,
+                    node.limit.map(|l| l as usize),
+                )?))
+            }
+            IndexLakePhysicalPlanType::Insert(node) => {
+                if inputs.len() != 1 {
+                    return Err(DataFusionError::Internal(format!(
+                        "IndexLakeInsertExec requires exactly one input, got {}",
+                        inputs.len()
+                    )));
+                }
+                let input = inputs[0].clone();
+
+                let table = load_table(&self.client, &node.namespace_name, &node.table_name)?;
+
+                let insert_op = parse_insert_op(node.insert_op)?;
+
+                Ok(Arc::new(IndexLakeInsertExec::try_new(
+                    Arc::new(table),
+                    input,
+                    insert_op,
+                )?))
+            }
+            // TODO upstream this to datafusion
+            IndexLakePhysicalPlanType::MemoryDatasource(node) => {
+                let partitions = parse_partitions(&node.partitions)?;
+                let schema = Schema::try_from(&node.schema.unwrap())?;
+                let projection = parse_projection(node.projection.as_ref());
+
+                let sort_information = node
+                    .sort_information
+                    .iter()
+                    .map(|sort_exprs| {
+                        let sort_exprs = parse_physical_sort_exprs(
+                            sort_exprs.physical_sort_expr_nodes.as_slice(),
+                            registry,
+                            &schema,
+                            self,
+                        )?;
+                        Ok::<_, DataFusionError>(sort_exprs)
+                    })
+                    .collect::<Result<Vec<_>, _>>()?;
+
+                let show_sizes = node.show_sizes;
+                let fetch = node.fetch.map(|f| f as usize);
+                let memory_source =
+                    MemorySourceConfig::try_new(&partitions, Arc::new(schema), projection)?
+                        .with_show_sizes(show_sizes)
+                        .with_limit(fetch);
+
+                let memory_source =
+                    MemorySourceConfig::try_with_sort_information(memory_source, sort_information)?;
+                Ok(DataSourceExec::from_data_source(memory_source))
+            }
         }
     }
 
@@ -83,13 +139,17 @@ impl PhysicalExtensionCodec for IndexLakePhysicalCodec {
 
             let filters = serialize_exprs(&exec.filters, &DefaultLogicalExtensionCodec {})?;
 
-            let proto = IndexLakeScanExecNode {
-                namespace_name: exec.table.namespace_name.clone(),
-                table_name: exec.table.table_name.clone(),
-                partition_count: exec.partition_count as u32,
-                projection,
-                filters,
-                limit: exec.limit.map(|l| l as u32),
+            let proto = IndexLakePhysicalPlanNode {
+                index_lake_physical_plan_type: Some(IndexLakePhysicalPlanType::Scan(
+                    IndexLakeScanExecNode {
+                        namespace_name: exec.table.namespace_name.clone(),
+                        table_name: exec.table.table_name.clone(),
+                        partition_count: exec.partition_count as u32,
+                        projection,
+                        filters,
+                        limit: exec.limit.map(|l| l as u32),
+                    },
+                )),
             };
 
             proto.encode(buf).map_err(|e| {
@@ -102,10 +162,14 @@ impl PhysicalExtensionCodec for IndexLakePhysicalCodec {
         } else if let Some(exec) = node.as_any().downcast_ref::<IndexLakeInsertExec>() {
             let insert_op = serialize_insert_op(exec.insert_op);
 
-            let proto = IndexLakeInsertExecNode {
-                namespace_name: exec.table.namespace_name.clone(),
-                table_name: exec.table.table_name.clone(),
-                insert_op,
+            let proto = IndexLakePhysicalPlanNode {
+                index_lake_physical_plan_type: Some(IndexLakePhysicalPlanType::Insert(
+                    IndexLakeInsertExecNode {
+                        namespace_name: exec.table.namespace_name.clone(),
+                        table_name: exec.table.table_name.clone(),
+                        insert_op,
+                    },
+                )),
             };
 
             proto.encode(buf).map_err(|e| {
@@ -115,6 +179,49 @@ impl PhysicalExtensionCodec for IndexLakePhysicalCodec {
             })?;
 
             Ok(())
+        } else if let Some(exec) = node.as_any().downcast_ref::<DataSourceExec>() {
+            let source = exec.data_source();
+            if let Some(memory_source) = source.as_any().downcast_ref::<MemorySourceConfig>() {
+                let proto_partitions = serialize_partitions(memory_source.partitions())?;
+                let projection = serialize_projection(memory_source.projection().as_ref());
+                let sort_information = memory_source
+                    .sort_information()
+                    .iter()
+                    .map(|ordering| {
+                        let sort_exprs = serialize_physical_sort_exprs(ordering.clone(), self)?;
+                        Ok::<_, DataFusionError>(
+                            datafusion_proto::protobuf::PhysicalSortExprNodeCollection {
+                                physical_sort_expr_nodes: sort_exprs,
+                            },
+                        )
+                    })
+                    .collect::<Result<Vec<_>, _>>()?;
+
+                let proto = IndexLakePhysicalPlanNode {
+                    index_lake_physical_plan_type: Some(
+                        IndexLakePhysicalPlanType::MemoryDatasource(MemoryDatasourceNode {
+                            partitions: proto_partitions,
+                            schema: Some(memory_source.original_schema().try_into()?),
+                            projection,
+                            sort_information,
+                            show_sizes: memory_source.show_sizes(),
+                            fetch: memory_source.fetch().map(|f| f as u32),
+                        }),
+                    ),
+                };
+
+                proto.encode(buf).map_err(|e| {
+                    DataFusionError::Internal(format!(
+                        "Failed to encode memory datasource node: {e:?}"
+                    ))
+                })?;
+
+                Ok(())
+            } else {
+                Err(DataFusionError::NotImplemented(format!(
+                    "IndexLakePhysicalCodec only support encoding MemorySourceConfig, got {source:?}",
+                )))
+            }
         } else {
             Err(DataFusionError::NotImplemented(format!(
                 "IndexLakePhysicalCodec does not support encoding {}",
@@ -166,4 +273,42 @@ fn parse_insert_op(insert_op: i32) -> Result<InsertOp, DataFusionError> {
         datafusion_proto::protobuf::InsertOp::Overwrite => Ok(InsertOp::Overwrite),
         datafusion_proto::protobuf::InsertOp::Replace => Ok(InsertOp::Replace),
     }
+}
+
+fn serialize_partitions(partitions: &[Vec<RecordBatch>]) -> Result<Vec<Vec<u8>>, DataFusionError> {
+    let mut proto_partitions = vec![];
+    for partition in partitions {
+        if partition.is_empty() {
+            proto_partitions.push(vec![]);
+            continue;
+        }
+        let mut proto_partition = vec![];
+        let mut stream_writer =
+            StreamWriter::try_new(&mut proto_partition, &partition[0].schema())?;
+        for batch in partition {
+            stream_writer.write(batch)?;
+        }
+        stream_writer.finish()?;
+        proto_partitions.push(proto_partition);
+    }
+    Ok(proto_partitions)
+}
+
+fn parse_partitions(
+    proto_partitions: &[Vec<u8>],
+) -> Result<Vec<Vec<RecordBatch>>, DataFusionError> {
+    let mut partitions = vec![];
+    for proto_partition in proto_partitions {
+        if proto_partition.is_empty() {
+            partitions.push(vec![]);
+            continue;
+        }
+        let mut partition = vec![];
+        let stream_reader = StreamReader::try_new(proto_partition.as_slice(), None)?;
+        for batch in stream_reader {
+            partition.push(batch?);
+        }
+        partitions.push(partition);
+    }
+    Ok(partitions)
 }
